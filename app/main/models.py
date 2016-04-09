184f87e1024da5c2cf619a54a4454652c23bf711
mooowooo <mooowooo@live.com>
2016-04-10 02:27:55 +0800

    [Fix](UserDetail): mod avatar path bug
    as title said

--- app/main/models.py
+++ app/main/models.py
@@ -43,3 +43,8 @@ class Career(db.Model):
     __tablename__ = 'careers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
+
+class Hometown(db.Model):
+    __tablename__ = 'hometown'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 from app.main.decorator import login_required_
 from flask import render_template
 from . import user
-from .models import User
+from .models import User, UserDetail
 
 __author__ = 'Jux.Liu'
 
@@ -11,4 +11,5 @@ __author__ = 'Jux.Liu'
 @login_required_
 def user_main(user_id):
     u = User.query.get(user_id)
-    return render_template('user_pages/user_detail.html', user=u)
+    ud = UserDetail.query.get(user_id)
+    return render_template('user_pages/user_detail.html', user=u, user_detail=ud)
