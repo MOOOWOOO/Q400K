94b193d2e370b188bdd14eca0f1832f7717d656c
mooowooo <mooowooo@live.com>
2016-05-08 15:31:33 +0800

    [Feature](journal): add pagenation
    add pagenation on web page

--- app/journal/views.py
+++ app/journal/views.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from flask import render_template
-from sqlalchemy import desc
+from sqlalchemy import desc,asc
+from math import ceil
 
 from app.main.decorator import login_required_
 from . import journal
@@ -9,8 +10,11 @@ from .models import Journal
 __author__ = 'Jux.Liu'
 
 
+@journal.route('/list/')
 @journal.route('/list/<int:page>/')
 @login_required_
-def list(page):
-    journal_list = Journal.query.order_by(desc(Journal.id)).offset(50 * (page - 1)).limit(50 * page).all()
-    return render_template('journal/list.html', journals=journal_list)
+def list(page=1):
+    journal_db = Journal.query
+    journal_pages = int(ceil(journal_db.count() / 50))
+    journal_list = journal_db.order_by(asc(Journal.id)).offset(50 * (page - 1)).limit(50).all()
+    return render_template('journal/list.html', journals=journal_list, number=page, total=journal_pages)
