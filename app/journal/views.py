44cacaea94dce421dc9c208b4eb66322a4c6cbcd
mooowooo <mooowooo@live.com>
2016-05-11 04:21:19 +0800

    [Fix](journal): fix a float devision error
    when total journals devide the number per page, the number is not float, so when it get the ceil result, it always not work

--- app/journal/views.py
+++ app/journal/views.py
@@ -16,7 +16,7 @@ __author__ = 'Jux.Liu'
 @login_required_
 def list(page=1):
     journal_db = Journal.query
-    journal_pages = int(ceil(journal_db.count() / 50))
+    journal_pages = int(ceil(journal_db.count() / 50.0))
     journal_list = journal_db.order_by(asc(Journal.id)).offset(50 * (page - 1)).limit(50).all()
     return render_template('journal/list.html', journals=journal_list, number=page, total=journal_pages)
 
