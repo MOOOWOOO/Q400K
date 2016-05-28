95c5475796f4e484eda7fc08761ac7252ad24861
mooowooo <mooowooo@live.com>
2016-05-28 17:41:07 +0800

    [Feature](js/util): add animation
    add animations.js to collect all js animations

---
+++ app/static/js/util/animations.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jux on 16-5-28.
+ */
+
+var waiting = function () {
+    this.html = "<img class='img-responsive center-block' src='/static/images/bird/bird_exercise.gif' />";
+};
