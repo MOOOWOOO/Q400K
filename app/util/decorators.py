92382e3538bc36964094dcf824da4bd365860ba4
mooowooo <mooowooo@live.com>
2016-06-01 19:41:26 +0800

    [Refacotr](decorator): move decorator from auth to util
    as title said

--- app/util/decorators.py
+++ app/util/decorators.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from functools import wraps
 
-from flask import current_app, redirect, url_for, request
+from flask import current_app, redirect, url_for, request, jsonify
 
 from flask.ext.login import current_user
 
@@ -22,5 +22,21 @@ def login_required_(func):
         elif not current_user.is_authenticated:
             return redirect(url_for("main.login", next=request.url))
         return func(*args, **kwargs)
+
+    return decorated_view
+
+
+def root_required(func):
+    """
+    检查是否是超级管理员
+    :param func:
+    :return:
+    """
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if current_user.role_id == 0:
+            return func(*args, **kwargs)
+        else:
+            return jsonify({'code': 3, 'msg': 'Permission denied, root required.'})
 
     return decorated_view
